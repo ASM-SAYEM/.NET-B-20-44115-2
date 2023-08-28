f209d158b4afb21a23e1d7bef9484c9fe7e9fc8f
sarah4176 <114296266+sarah4176@users.noreply.github.com>
2023-08-29 04:11:04 +0600

    .

---
+++ DAL/DataAccessFactory1.cs
@@ -0,0 +1,66 @@
+﻿using DAL.EF.Models;
+using DAL.Interfaces;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<Payment, int, bool> PaymentData()
+        {
+            return new PaymentRepo();
+        }
+        public static IRepo<Client, int, bool> ClientData()
+        {
+            return new ClientRepo();
+        }
+        public static IRepo<Booking, int, bool> BookingData()
+        {
+            return new BookingRepo();
+        }
+        public static IRepo<Schedule, int, bool> ScheduleData()
+        {
+            return new ScheduleRepo();
+        }
+        public static IRepo<SrviceProvider, int, bool> SrviceProviderData()
+        {
+            return new SrviceProviderRepo();
+        }
+        public static IRepo<Issue, int, bool> IssueData()
+        {
+            return new IssueRepo();
+        }
+        public static IRepo<Manager, int, bool> ManagerData()
+        {
+            return new ManagerRepo();
+        }
+        public static IRepo<Token, int, bool> TokenData()
+        {
+            return new TokenRepo();
+        }
+        public static IRepo<BookingNotification, int, bool> BookingNotificationData()
+        {
+            return new BookingNotificationRepo();
+        }
+        public static IRepo<IssueNotification, int, bool> IssueNotificationData()
+        {
+            return new IssueNotificationRepo();
+        }
+        public static IRepo<PaymentNotification, int, bool> PaymentNotificationData()
+        {
+            return new PaymentNotificationRepo();
+        }
+
+        public static IAuth AuthData()
+        {
+            return new ManagerRepo();
+        }
+
+    }
+}

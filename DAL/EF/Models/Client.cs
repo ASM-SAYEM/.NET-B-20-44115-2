f85bec613b09e9e46928529bf6bf94df6b3f34db
ASM-SAYEM <114211918+ASM-SAYEM@users.noreply.github.com>
2023-08-29 11:06:13 +0600

    Update Client.cs

--- DAL/EF/Models/Client.cs
+++ DAL/EF/Models/Client.cs
@@ -13,12 +13,20 @@ namespace DAL.EF.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        public string UserName {get; set;}
+        [Required]
         public string Name { get; set; }
         [Required]
         [MinLength(8)] 
         public string Password { get; set; }
         [Required]
-        public int PhoneNumber { get; set; }
+        public int PhoneNumber { get; set; } 
+        [Required]
+        public string Type {get;set}
+        
+        [Required]
+        public string Email {get;set}
+        
        
 
     }
